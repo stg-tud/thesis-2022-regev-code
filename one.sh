ddbc4a55bd7d4a9dc7b41f24cacab3772d4762cd
Roey <33815022+SaltyHash123@users.noreply.github.com>
2022-11-04 12:23:34 +0100

    Initial Commit with compile.bat fix

---
+++ one.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$(uname)" != "Linux" ]; then
+    SCRIPT_DIR=$(dirname $(perl -MCwd -e 'print Cwd::abs_path shift' "$0"))
+else
+    SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+fi
+
+echo "Starting the ONE from $SCRIPT_DIR"
+java -Xmx4096M -cp $SCRIPT_DIR/target:$SCRIPT_DIR/lib/ECLA.jar:$SCRIPT_DIR/lib/DTNConsoleConnection.jar:$SCRIPT_DIR core.DTNSim $*
