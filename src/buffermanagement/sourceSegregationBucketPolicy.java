6d253a35b1bafc6eb998f802d2aa98159eb5adac
Roey <33815022+SaltyHash123@users.noreply.github.com>
2023-02-20 18:15:40 +0100

    change example policy to useful one
    prioritize own messages

--- src/buffermanagement/sourceSegregationBucketPolicy.java
+++ src/buffermanagement/sourceSegregationBucketPolicy.java
@@ -8,16 +8,16 @@ public class sourceSegregationBucketPolicy extends BucketAssignmentPolicy {
 
     public sourceSegregationBucketPolicy(Settings s){
         super(s);
-        this.BucketCount = 1;
+        this.BucketCount = 2;
     }
 
     @Override
     public Integer assignBucket(Message m, DTNHost currentHost, Boolean receivedMessage) {
-        if(m.getFrom().getAddress() % 2 == 0 || m.getFrom().equals(currentHost)){
+        if(m.getFrom().equals(currentHost)){
             return 0;
         }
         else{
-            return -1;
+            return 1;
         }
     }
     
