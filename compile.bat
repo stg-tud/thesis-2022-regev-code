88faeea341e2ef6b5351516e3b765ac53b50e142
Roey <33815022+SaltyHash123@users.noreply.github.com>
2023-03-01 12:01:44 +0100

    Make the-one executeable for every windwos path

--- compile.bat
+++ compile.bat
@@ -1,12 +1,12 @@
-set targetdir=target
+set targetdir=%~dp0target
 
 IF NOT EXIST "%targetdir%" mkdir %targetdir%
 
-javac -sourcepath src -d %targetdir% -cp lib/ECLA.jar;lib/DTNConsoleConnection.jar; src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java src/buffermanagement/*.java src/sendingpolicy/*.java src/droppolicy/*.java
+javac -sourcepath %~dp0src -d %targetdir% -cp %~dp0lib/ECLA.jar;%~dp0lib/DTNConsoleConnection.jar; %~dp0src/core/*.java %~dp0src/movement/*.java %~dp0src/report/*.java %~dp0src/routing/*.java %~dp0src/gui/*.java %~dp0src/input/*.java %~dp0src/applications/*.java %~dp0src/interfaces/*.java %~dp0src/buffermanagement/*.java %~dp0src/sendingpolicy/*.java %~dp0src/droppolicy/*.java
 
 
 
 IF NOT EXIST "%targetdir%\gui\buttonGraphics" (
 	mkdir %targetdir%\gui\buttonGraphics
-	copy src\gui\buttonGraphics\* %targetdir%\gui\buttonGraphics\
+	copy %~dp0src\gui\buttonGraphics\* %targetdir%\gui\buttonGraphics\
 )
